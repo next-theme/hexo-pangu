f77d87d735d20fc8c8551935e610716aaa090370
Mimi <1119186082@qq.com>
2020-04-02 19:50:25 +0800

    Split file

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# Hexo Filter Pangu
+# Hexo Pangu
 
-![Package Version](https://img.shields.io/github/package-json/v/theme-next/hexo-filter-pangu?style=flat-square)
+![Package Version](https://img.shields.io/github/package-json/v/theme-next/hexo-pangu?style=flat-square)
 
 Server side [pangu.js](https://github.com/vinta/pangu.js) filter for Hexo.
 
 ## Install
 
 ```bash
-npm install theme-next/hexo-filter-pangu
+npm install hexo-pangu
 ```
--- index.js
+++ index.js
@@ -2,19 +2,10 @@
 
 'use strict';
 
-const { JSDOM } = require('jsdom');
-const dom = new JSDOM();
-const { window } = dom;
-const { document, Node, DocumentFragment, XPathResult } = window;
-global.document = document;
-global.Node = Node;
-global.DocumentFragment = DocumentFragment;
-global.XPathResult = XPathResult;
+const spacing = require('./lib/filter');
 const pangu = require('pangu/src/browser/pangu');
 
 hexo.extend.filter.register('after_post_render', data => {
-  document.body.innerHTML = data.content;
-  pangu.spacingPageBody();
-  data.content = document.body.innerHTML;
+  data.content = spacing(data.content);
   data.title = pangu.spacing(data.title);
 }, 8);
