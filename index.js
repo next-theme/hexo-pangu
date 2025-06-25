08d7222d25247194ef7b78ca73ce8c036735c360
Mimi <1119186082@qq.com>
2025-06-25 23:12:15 +0800

    Fix import

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 'use strict';
 
 const spacing = require('./lib/filter');
-const pangu = require('pangu/src/browser/pangu');
+const { pangu } = require('pangu');
 
 hexo.extend.filter.register('after_post_render', data => {
   data.content = spacing(data.content);
